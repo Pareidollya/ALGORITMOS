75ac559d84b28a1e24024ecd67f6d0c457b932ae
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2025-09-04 10:45:34 -0300

    merge porém eh o insertion mais tryhard do mundo

--- sort.py
+++ sort.py
@@ -2,21 +2,7 @@ import time
 import math
 from typing import TypeAlias
 
-original = [
-    64,
-    25,
-    12,
-    22,
-    11,
-    5,
-    2,
-    7,
-    9,
-    3,
-    5,
-    # 78,
-    # 0,
-]
+original = [64, 25, 22, 12, 11, 5, 7, 3, 31, 57, 89, 10]
 
 SortResult: TypeAlias = tuple[list[int], int, str]
 
@@ -88,52 +74,91 @@ def merge(arr: list[int]) -> SortResult:
 
     l_size = len(arr) // 2
     r_size = len(arr[l_size:])  # slice start index
+    max_it = l_size
 
     # dividir
     L = arr[:l_size]
-    R = arr[r_size - 1 :]
+    R = arr[l_size:]
 
-    def swap(swap_arr):  # TODO: tem q fazer um swap-back para estar tudo ordenado
-        for a in range(len(swap_arr) - 1):
+    # first swaps
+    def swap(swap_arr):
+        a = 0
+        while a < (len(swap_arr) - 1):
             if swap_arr[a] > swap_arr[a + 1]:
                 swap_arr[a], swap_arr[a + 1] = swap_arr[a + 1], swap_arr[a]
+            a += 2
         return swap_arr
-
-    def merge(arr_l, arr_r):
-        merged = []
-
-        l = 0
-        r = 0
-        while r < len(arr_r):
-            # verificar se sobrou, quanto sobrou e adiciona-los a lista
-            if l > len(arr_l):  # é pq acabou l, adiciona sobra de r
-                merged.append(arr_r[r + 1])
-                break
-            elif r > len(arr_r):
-                merged.append(arr[l + 1])
-                break
-
-            if arr_l[l] > arr_r[r]:
-                merged.append(arr_r[r])
-                r += 1
-            else:
-                merged.append(arr_l[l])
-                l += 1
-        # adicionar oq sobrar
-        if l < len(arr_l) - 1:
-            for l_ in arr_l[l:]:
-                merged.append(l_)
-        if r < len(arr_r) - 1:
-            for r_ in arr_r[r:]:
-                merged.append(r_)
-
-        return merged
 
     L = swap(L)
     R = swap(R)
-    # first swaps
 
-    return merge(L, R), (ns() - start), "MERGE NAO-RECURSIVO"
+    # CONQUEEEEEEEEEEER
+    # FOR LEN / 2 ( aumentando array e ir conquistando essa poha)
+    def conq(arr):
+        final_arr = []
+        # os primeiros ja estarão ordenados
+        # aumentando o tamanho do bloco principal e reduzindo dos proximos
+        # comparar o primeiro elemento do principal com o primeiro do proximo bloco
+
+        conquested = [arr[0]]
+        l, r = 0, 1
+        atual = 0
+        while len(conquested) < len(
+            arr
+        ):  # aumentou area conquistada ate se igualar ao tamanho total
+
+            new_conquested = []
+            appends = 0
+            while atual < len(
+                conquested
+            ):  # TODO SO PRECISA ADICIONAR UM POR VEEEEEEEEEEEEEEEEEZ PERCORRENDO OS JA CONQUISTADO E INSERIR NA POSIÇÃO CORRETA
+                # basicamente focar em reescrever conquest com a posição correta
+
+                if conquested[atual] > arr[r]:  # se o proximo for menor que o atual
+                    new_conquested.append(arr[r])  # adiciona o proximo antes do atual
+                    conquested = (
+                        conquested[:atual] + new_conquested + conquested[atual:]
+                    )
+                    r += 1  # avança proximo
+                    atual = 0
+                    break
+
+                elif conquested[atual] < arr[r]:  # se atual for menor que o proximo
+                    # new_conquested.append(conquested[atual])  # adiciona o atual
+
+                    if (
+                        atual < len(conquested) - 1
+                    ):  # se o atual nao for o ultimo da lista avança o atual
+                        # pra comparar o proximo atual com r
+                        atual += 1
+                        break
+
+                    conquested = (
+                        conquested[: atual + 1] + [arr[r]] + conquested[atual + 1 :]
+                    )
+
+                    r += 1
+                    atual = 0
+                    break
+
+                if appends > len(
+                    conquested
+                ):  # se houver mais adiçoes que a lista atual adicionar quem sobra de conquest a partir do atual no final
+                    new_conquested.append(arr[r])
+                    conquested = new_conquested + conquested[atual:]
+                    break
+
+                if atual == len(
+                    conquested
+                ):  # se nao tiver um proximo atual adiciona proximo ao fim
+                    new_conquested.append(arr[r])
+                    r += 1
+                    conquested = new_conquested
+                    break
+
+        return conquested
+
+    return conq(L + R), (ns() - start), "MERGE NAO-RECURSIVO (CAVALOKKKK)"
 
 
 showResults(
