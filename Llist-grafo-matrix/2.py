522a66cda3113e0e7c3236ffd72b5329b223795d
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2025-09-09 19:15:11 -0300

    busca largura e profundidade

--- Llist-grafo-matrix/2.py
+++ Llist-grafo-matrix/2.py
@@ -69,7 +69,8 @@ class Grafo:
         for i in range(len(self.matrix)):
             v = ""
             for j in range(len(self.matrix[i])):
-                v += f'{j if self.matrix[i][j] > 0 else ""} '
+                if self.matrix[i][j] > 0:
+                    v += f"{j} "
             print(f"{vertice_0} -> ({v})")
             vertice_0 += 1
 
@@ -83,14 +84,89 @@ class Grafo:
             vistos.append(i)
         return a
 
+    # busca largura. FILA
+    # visita vizinhos do vertice inicial, em seguida os vizinhos do vizinho, funciona com uma fila.
+    # vertice inicial v, aciciona v a fila.
+    # Enquanto fila nao estiver vazia - reritar primeiro vertice da fila e o marca como visitado (outra lista).
+    #                                   - Marca seus vizinhos como visitado e adiciona-os a fila
 
-a = Grafo()
-a.addVertice()
-a.addVertice()
-a.addVertice()
-a.linkFromArr(0, [1, 2])
-a.link(2, 1)
-# a.link(1, 1)
-a.showMatrix()
-a.getGraph()
-print(a.getArestasReg(), "Arestas")
+    def buscaLargura(self, v_inicio) -> list[int]:
+        viwed = [v_inicio]
+        queue = [v_inicio]
+
+        while len(queue) > 0:
+            actual = queue.pop(0)  # remove vertice atual
+            # marcar vizinhos como visto
+            for i in range(len(self.matrix[actual])):
+                vizinho = self.matrix[actual][i]
+                if i not in viwed and vizinho > 0:
+                    viwed.append(i)
+                    queue.append(i)
+        return viwed
+
+    def buscaProfundidade(self, v_inicio):
+        viwed = []
+        stack = [v_inicio]
+
+        while len(stack) > 0:
+            actual = stack.pop(len(stack) - 1)  # remove vertice atual do topo da pilha
+            if actual not in viwed:
+                viwed.append(actual)
+
+                # adicionar vizinhos nao visitados
+                for i in range(len(self.matrix[actual])):
+                    vizinho = self.matrix[actual][i]
+                    if i not in viwed and vizinho > 0:
+                        stack.append(i)
+        return viwed
+
+
+# a = Grafo()
+# a.addVertice()
+# a.addVertice()
+# a.addVertice()
+# a.linkFromArr(0, [1, 2])
+# a.link(2, 1)
+# # a.link(1, 1)
+# a.showMatrix()
+# a.getGraph()
+# print(a.getArestasReg(), "Arestas")
+
+print("\n\n\n")
+# g3a = Grafo()
+# for v in range(11):
+#     g3a.addVertice(show=False)
+
+# g3a.linkFromArr(0, [1, 2, 3])
+# g3a.linkFromArr(1, [0, 2, 4])
+# g3a.linkFromArr(2, [0, 1, 3])
+# g3a.linkFromArr(3, [0, 2, 6])
+
+# g3a.linkFromArr(4, [1, 5, 7])
+# g3a.linkFromArr(5, [4, 6, 8])
+# g3a.linkFromArr(6, [3, 5, 9])
+
+# g3a.linkFromArr(7, [4, 8, 10])
+# g3a.linkFromArr(8, [7, 5, 9])
+# g3a.linkFromArr(9, [6, 8, 10])
+
+# g3a.linkFromArr(10, [7, 9])  # aq ó
+# g3a.showMatrix()
+# g3a.getGraph()
+# print(g3a.getArestasReg(), "Arestas")
+
+
+g = Grafo()
+for v in range(5):
+    g.addVertice(show=False)
+
+g.linkFromArr(0, [1, 3])
+g.linkFromArr(1, [0, 2, 4])
+g.linkFromArr(2, [1])
+g.linkFromArr(3, [0, 4])
+g.linkFromArr(4, [1, 3])
+g.showMatrix()
+g.getGraph()
+print(g.getArestasReg(), "Arestas")
+print(g.buscaLargura(0))
+print(g.buscaProfundidade(0))
